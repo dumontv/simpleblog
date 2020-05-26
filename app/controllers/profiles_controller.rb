1f89bd95de9f392ab81b521680bc59ec9730b516
dumontv <dumontv@outlook.com>
2020-05-26 00:14:35 -0400

    Add profile feature

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,44 @@
+class ProfilesController < ApplicationController
+  def index
+    @profiles = Profile.all
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+
+    if(@profile.save)
+      redirect_to profiles_path
+    end
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+
+    if(@profile.update(profile_params))
+      redirect_to profiles_path
+    end
+  end
+
+  def destroy
+    @profile = Profile.find(params[:id])
+    @profile.destroy
+    
+    redirect_to profiles_path
+  end
+
+  private def profile_params
+    params.require(:profile).permit(:username, :avatar)
+  end
+end
